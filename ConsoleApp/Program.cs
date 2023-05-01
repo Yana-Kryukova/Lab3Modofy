20f66dedfeb1df94f6c5cc22e244bef305188eb1
Yana-Kryukova <krjanik@icloud.com>
2023-05-01 13:24:52 +0700

    Start

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter A, B, +, -, * or /");
+string s = Console.ReadLine();
+int A = 0, B = 0;
+double res;
+switch (s)
+{
+    case "A": A = int.Parse(Console.ReadLine()); break;
+    case "B": B = int.Parse(Console.ReadLine()); break;
+    case "+": OperationPlus(A, B); break;
+    case "-": OperationMinus(A, B); break;
+    case "*": OperationMulty(A, B); break;
+    case "/": break;
+}
